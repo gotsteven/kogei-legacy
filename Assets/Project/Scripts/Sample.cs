39f4a5f4c586b9295d6507632d5b807911084b54
GotSteven <s1f102300481@iniad.org>
2025-09-17 04:34:23 +0900

    ディレクトリ構成の変更

---
+++ Assets/Project/Scripts/Sample.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, Unity!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello, Unity!");
+    }
+}
