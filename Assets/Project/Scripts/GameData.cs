81943a7c7876af7de7324ed0543b927599fc6e56
GotSteven <s1f102300481@iniad.org>
2025-11-01 15:42:49 +0900

    工房内の基本的な画面遷移

---
+++ Assets/Project/Scripts/GameData.cs
@@ -0,0 +1,8 @@
+public static class GameData
+{
+    // ミニゲーム完了フラグ
+    public static bool isCraftCompleted = false;
+    
+    // どの工芸品が完成したか（任意）
+    public static string completedCraftName = "";
+}
