4f193efe207f6ceff45623f601ffc26b7328612a
Sor03-JP <soraaa2017@gmail.com>
2026-01-14 02:07:27 +0900

    feat: パララックス背景の実装

---
+++ Assets/Project/Scripts/ParallaxEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    public GameObject cam;
+    public float parallaxEffect;
+
+    private float length;
+    private float startPos;
+
+    void Start()
+    {
+        if (cam == null) cam = Camera.main.gameObject;
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void LateUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
+    }
+}
