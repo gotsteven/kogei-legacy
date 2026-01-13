187c7126d2a9694124d716e325b4def65b8987af
Sor03-JP <soraaa2017@gmail.com>
2026-01-14 01:05:50 +0900

    feat: 時間経過による環境光変化(Day/Night)の実装

---
+++ Assets/Project/Scripts/Time/DayNightLighting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DayNightLighting : MonoBehaviour
+{
+    [Header("設定")]
+    public Light2D globalLight; // シーン全体を照らすライト
+
+    [Header("色の移り変わり設定")]
+    [Tooltip("左端が0:00(夜)、真ん中が12:00(昼)、右端が24:00(夜)")]
+    public Gradient dayNightGradient; // グラデーション設定用
+
+    private void Update()
+    {
+        if (TimeManager.Instance == null) return;
+        if (globalLight == null) return;
+
+        // 現在の時間を取得
+        float time = TimeManager.Instance.normalizedTime;
+
+        Color currentColor = dayNightGradient.Evaluate(time);
+
+        // ライトの色に反映させる
+        globalLight.color = currentColor;
+    }
+}
